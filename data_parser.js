da7038af1a8933acb84890f66492078a5fe8c552
archmagos-dominus <drobertc@protonmail.com>
2022-04-29 22:36:19 +0300

    final

--- data_parser.js
+++ data_parser.js
@@ -1,15 +1,19 @@
-//encode data from the history of conversation (./datasets/livedata.json) to the main >
-//> trainig dataset (./datasets/dataset.csv)
+//encode data from the history of conversation (./convos.json) to the main >
+//> trainig dataset (./lines.csv)
 
 //import fs module to read/write from files
 const fs = require('fs');
 //import conversation data json
-const cvs = require('./datasets/livedata.json');
+var cvs = require('./datasets/livedata.json');
+//import dataset file
+//var dataset = require('./datasets/dataset.csv')
+//this is what you get for not making livedata an array mlem
+var line_counter = 0;
 
 //iterate thought the conversation data
 for (let i in cvs) {
   //create first dialogue
-  //format: `[NAME],"dialogue line"`
+  //format: `NAME,"dialogue line"`
   line1 = cvs[i].username + ',' + '"' + cvs[i].userquote + '"';
   //append created line to dataset
   fs.appendFileSync('./datasets/dataset.csv', `\n${line1}`, 'utf-8');
@@ -18,4 +22,7 @@ for (let i in cvs) {
   line2 = 'Hourai' + ',' + '"' + cvs[i].botreply + '"';
   //append created line to dataset
   fs.appendFileSync('./datasets/dataset.csv', `\n${line2}`, 'utf-8');
+  line_counter++;
 }
+
+console.log(`Successfully parsed ${line_counter} messages to the main dataset.`)
